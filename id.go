0acf69bac167fe8489026eb212e7a343d64c555b
rtpchan <rtpchan@hotmail.com>
2023-12-08 15:45:01 -0600

    defer ID unlock

--- id.go
+++ id.go
@@ -16,12 +16,19 @@ func NewIDGenerator() *ID {
 func (i *ID) NewID() int {
 	i.lock.Lock()
 	i.current++
-	i.lock.Unlock()
+	defer i.lock.Unlock()
 	return i.current
 }
 
 func (i *ID) Reset() {
 	i.lock.Lock()
 	i.current = 0
+	i.lock.Unlock()
+}
+
+// Up to the caller to ensure id is still unique
+func (i *ID) SetCurrent(n int) {
+	i.lock.Lock()
+	i.current = n
 	i.lock.Unlock()
 }
